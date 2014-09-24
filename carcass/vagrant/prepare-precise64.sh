d9c2aa8bac6c5df32e1649cabb663f5694fd34fb
Mihail Shumilov <mschumilow@gmail.com>
2014-09-24 16:40:32 +0300

    Update prepare-precise64.sh

--- carcass/vagrant/prepare-precise64.sh
+++ carcass/vagrant/prepare-precise64.sh
@@ -37,7 +37,7 @@ rm -rf /etc/apache2/sites-enabled/*
 # Enabling mod_rewrite on server
 ## `cd` is just for symlink to have same format as others inside `mods-enabled/`
 cd /etc/apache2/mods-enabled/
-ln -s ../mods-available/rewrite.load rewrite.load
+ln -sn ../mods-available/rewrite.load rewrite.load
 
 # Make the apache load under our user account.
 sed -ri 's/^(export APACHE_RUN_USER=)(.*)$/\1vagrant/' /etc/apache2/envvars
@@ -47,7 +47,7 @@ sed -ri 's/^(export APACHE_RUN_GROUP=)(.*)$/\1vagrant/' /etc/apache2/envvars
 touch /etc/apache2/conf-available/globalname.conf
 echo "ServerName boilerplate" > /etc/apache2/conf-available/globalname.conf
 cd /etc/apache2/conf-enabled
-ln -s ../conf-available/globalname.conf globalname.conf
+ln -sn ../conf-available/globalname.conf globalname.conf
 
 # Creating database
 mysql -u root -p${DB_PASS} -e "create database if not exists ${DB_NAME}";
